cf7da7d35786e36c2a23e22d0de3ea209e8094d2
Richal Aleman <richalaleman@gmail.com>
2026-08-01 11:24:31 +0900

    chore(formula): bump utmd to v0.1.14

--- utmd.rb
+++ utmd.rb
@@ -1,13 +1,13 @@
 class Utmd < Formula
   desc "Minimalist developer sandbox and disposable VMs for UTM on macOS"
   homepage "https://github.com/tappunk/utmd"
-  version "0.1.13"
+  version "0.1.14"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/tappunk/utmd/releases/download/v#{version}/utmd-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "15eb868dd97d1f5c7b4d46fba47d1eefc70dff4bfdc4bcad1daddfb6b26ad0d1"
+  sha256 "03d26df32f50226ca1ae23784ad957d6d90ba965308b186adc02ea32352eba09"
 
   def install
     bin.install "utmd"
