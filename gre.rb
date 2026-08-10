1dbd8f72795adf05747df837adcb8dce36d5ab95
Richal Aleman <richalaleman@gmail.com>
2026-08-10 15:15:13 +0900

    chore(formula): bump gre to v0.1.15

--- gre.rb
+++ gre.rb
@@ -2,13 +2,13 @@ class Gre < Formula
   desc "A super-fast multi-repo git recap for AI agents and speed-obsessed humans"
   homepage "https://github.com/richalaleman/gre"
   license "MIT"
-  version "0.1.14"
+  version "0.1.15"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/richalaleman/gre/releases/download/v#{version}/gre-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "02465361d9f0c8fca6af8af112c51dbf6aaf80a3cd5313800ebd3744ffd3f48a"
+  sha256 "2f7ae7813d9fc1d244cd2f962fb91a3e04d4de7d3bef23049aa26ef3a4bd6af3"
 
   def install
     bin.install "gre"
