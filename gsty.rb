43502ff2c09615f3b9c7ea6e088077dc329ca824
Richal Aleman <richalaleman@gmail.com>
2026-08-10 15:19:45 +0900

    chore(formula): bump gsty to v0.1.12

--- gsty.rb
+++ gsty.rb
@@ -2,13 +2,13 @@ class Gsty < Formula
   desc "Ghostty live preview theme browser and installer TUI"
   homepage "https://github.com/richalaleman/gsty"
   license "MIT"
-  version "0.1.11"
+  version "0.1.12"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/richalaleman/gsty/releases/download/v#{version}/gsty-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "7c9747dca3a8340781eb3c4476637a03620172320317d062f54b5bac6a18fa5c"
+  sha256 "f50f8456230e4082db8ffb8c5145c282e1e4417db487271ba7a97c693ee0edd3"
 
   def install
     bin.install "gsty"
