127c57d64b55b43c0ba6110454fdc307d14e1ed6
Richal Aleman <richalaleman@gmail.com>
2026-08-10 15:26:43 +0900

    chore(formula): bump tnk to v0.1.39

--- tnk.rb
+++ tnk.rb
@@ -1,14 +1,14 @@
 class Tnk < Formula
   desc "Zero-trust sandbox for local inference and secure AI coding agent runtimes"
   homepage "https://richalaleman.github.io"
-  version "0.1.38"
+  version "0.1.39"
 
   depends_on arch: :arm64
   depends_on "lima"
   depends_on "llama.cpp"
 
   url "https://github.com/richalaleman/tnk/releases/download/v#{version}/tnk-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "fbae9441a62b0b47d7d7f37183e87a952bd5307802a526dcf214c5ebb90e9f2b"
+  sha256 "9e17c151b2c60660266b5bf1b1004401ea6549b02bfb7b39af08f38357b39174"
 
   def install
     bin.install "tnk"
