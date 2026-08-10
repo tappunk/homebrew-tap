78f1811a46d646ffa339cd4ddb8d4a10c9045aa9
Richal Aleman <richalaleman@gmail.com>
2026-08-10 15:10:14 +0900

    chore(formula): bump utmd to v0.1.16

--- utmd.rb
+++ utmd.rb
@@ -2,13 +2,13 @@ class Utmd < Formula
   desc "Minimalist developer sandbox and disposable VMs for UTM on macOS"
   homepage "https://github.com/richalaleman/utmd"
   license "MIT"
-  version "0.1.15"
+  version "0.1.16"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/richalaleman/utmd/releases/download/v#{version}/utmd-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "3627e4cfa9ff0ebfbaabd9c71f2e22d2b38d4ff9757fc4d02812ff667f9214a9"
+  sha256 "2873502998efbe51dc82473a0a443664da8bb9ea7ec8e6c6e9ab0db957b64699"
 
   def install
     bin.install "utmd"
