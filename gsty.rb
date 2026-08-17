20429d110b9479c37c4392607c44b726cb7616fe
Richal Aleman <richal@tappunk.com>
2026-08-17 15:44:51 +0900

    chore(formula): bump gsty to v0.1.13

--- gsty.rb
+++ gsty.rb
@@ -2,13 +2,13 @@ class Gsty < Formula
   desc "Ghostty live preview theme browser and installer TUI"
   homepage "https://github.com/tappunk/gsty"
   license "MIT"
-  version "0.1.12"
+  version "0.1.13"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/tappunk/gsty/releases/download/v#{version}/gsty-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "f50f8456230e4082db8ffb8c5145c282e1e4417db487271ba7a97c693ee0edd3"
+  sha256 "ace8ddfb09c59b9499236b88450b7e8ec5429efbe1aa2640f2f5fd2cbc27c108"
 
   def install
     bin.install "gsty"
