46dd04953b025a0ab0f321e3412f016591bece02
Richal Aleman <me@tappunk.com>
2026-09-14 23:49:53 +0900

    chore(formula): bump tnk to v0.1.51

--- tnk.rb
+++ tnk.rb
@@ -1,13 +1,13 @@
 class Tnk < Formula
   desc "Zero-trust sandbox for local inference and secure AI coding agent runtimes"
   homepage "https://tappunk.com"
-  version "0.1.50"
+  version "0.1.51"
 
   depends_on arch: :arm64
   depends_on "lima"
 
   url "https://github.com/tappunk/tnk/releases/download/v#{version}/tnk-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "17e12eac46492bd8a93eeb4bf26006dd5ee8f35a0b16ecea634487822da15417"
+  sha256 "4aaf9acab3ef77e25f40a5cd5a11179ec0b63bed711504ae827e9818b3732771"
 
   def install
     bin.install "tnk"
