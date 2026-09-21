c71e5d9336b3810cc106985c12244ce5228cc8e6
tappunk <me@tappunk.com>
2026-09-21 23:06:49 +0900

    feat(utmd): add utmd formula

---
+++ utmd.rb
@@ -0,0 +1,20 @@
+class Utmd < Formula
+  desc "Minimalist developer sandbox and disposable VMs for UTM on macOS"
+  homepage "https://github.com/tappunk/utmd"
+  license "MIT"
+  version "0.1.25"
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  url "https://github.com/tappunk/utmd/releases/download/v#{version}/utmd-#{version}-bin-macos-arm64.tar.gz"
+  sha256 "acce901d28a8bdc4f3763393e39f7c427510932dfdc25ba4d4730fa5c3689f36"
+
+  def install
+    bin.install "utmd"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/utmd --version")
+  end
+end
