eb2862b5ab614053f222d186157fa2a333bd34c0
tappunk <me@tappunk.com>
2026-09-24 14:45:40 +0900

    chore(formula): bump gre to v0.1.25

--- gre.rb
+++ gre.rb
@@ -2,13 +2,13 @@ class Gre < Formula
   desc "A super-fast multi-repo git recap for AI agents and speed-obsessed humans"
   homepage "https://github.com/tappunk/gre"
   license "MIT"
-  version "0.1.24"
+  version "0.1.25"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://github.com/tappunk/gre/releases/download/v#{version}/gre-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "38f103faafee5bdc315b3574c7e164f691cf3616f5c1b37c914be16af80e2e70"
+  sha256 "89db109df007ea8897af4afac7a390c85910b555c15d3d4ac3b84226dccedb1d"
 
   def install
     bin.install "gre"
